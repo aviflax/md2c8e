25636af24e612e541b5634251c739d3b9b44dd6b
Avi Flax <avi.flax@fundingcircle.com>
2020-05-21 20:06:47 -0400

    Add exception handling to more calls to Confluence
    I was testing this locally and an exception bubbled out of this
    namespace, aborting my entire workflow. That helped me realize that I’d
    missed a few calls when I added exception handling in the prior commit.
    
    I’ll test this in a few minutes. It’ll probably work fine, but the
    end-result of the two commits combined has me rethinking the approach.
    It’s a lot of code, and it harms readability and thus maintainability.
    
    And I think I see a potential simpler approach: to change the hato
    config to throw exceptions, and then we can handle *all* failures the
    same way: by converting the exception to a fault. That’d be simpler than
    the current approach, wherein many of the operations have two different
    error-handling pathways: one for a request that wasn’t successful but no
    exception was thrown, and one for exceptions (e.g. timeouts, DNS
    failures, etc.)

--- src/md2c8e/confluence.clj
+++ src/md2c8e/confluence.clj
@@ -51,7 +51,13 @@
   [res]
   (when-not (successful? res)
     (fault ::response res
-           ::anom/message (get-in res [:body :message]))))
+           ::anom/message (and (map? res) (get-in res [:body :message])))))
+
+(defn- exception->fault
+  [e]
+  (merge (ex-data e)
+         (fault ::anom/message (str e)
+                :exception e)))
 
 (defn- get-page-by-id
   "Returns the page with the supplied ID, or an ::anom/anomaly.
@@ -59,14 +65,16 @@
     * :not-found — the page simply doesn’t exist
     * :fault — something went wrong"
   [page-id {:keys [::req-opts ::url] :as _client}]
-  (let [res (hc/get (url :content page-id) (assoc req-opts :query-params {:expand "version,space"}))]
-    (case (:status res)
-      200 (:body res)
-      404 {::anom/category :not-found
-           ::anom/message (format "No page with ID %s exists" page-id)
-           ::page-id page-id
-           ::response res}
-      (response->fault res))))
+  (let [res (try (hc/get (url :content page-id) (assoc req-opts :query-params {:expand "version,space"}))
+                 (catch Exception e (exception->fault e)))]
+    (or (anom res)
+        (case (:status res)
+          200 (:body res)
+          404 {::anom/category :not-found
+               ::anom/message (format "No page with ID %s exists" page-id)
+               ::page-id page-id
+               ::response res}
+          (response->fault res)))))
 
 (defn page-exists?!
   "Returns nil if the page exists; throws an Exception if it does not."
@@ -77,14 +85,16 @@
 (defn- get-page-by-title
   "Get the page with the supplied title, or nil if no such page is found."
   [title space-key {:keys [::req-opts ::url] :as _client}]
-  (let [res (hc/get (url :content) (assoc req-opts :query-params {:spaceKey space-key
-                                                                  :title title
-                                                                  :expand "version"}))
+  (let [res (try (hc/get (url :content) (assoc req-opts :query-params {:spaceKey space-key
+                                                                       :title title
+                                                                       :expand "version"}))
+                 (catch Exception e (exception->fault e)))
         results (get-in res [:body :results])]
-    (if (and (= (:status res) 200)
-             (<= (count results) 1))
-      (first results) ; will either return the page, if present, or nil
-      (response->fault res))))
+    (or (anom res)
+        (if (and (= (:status res) 200)
+                 (<= (count results) 1))
+          (first results) ; will either return the page, if present, or nil
+          (response->fault res)))))
 
 (defonce ^{:private true, :doc "Atom containing a map of page IDs to space keys, for caching."}
   page-id->space-key
@@ -97,12 +107,6 @@
             key (get-in res [:space :key])]
         (or (anom res)
             (swap! page-id->space-key assoc page-id key)))))
-
-(defn- exception->fault
-  [e]
-  (merge (ex-data e)
-         (fault ::anom/message (str e)
-                :exception e)))
 
 (defn- update-page
   [{id           :id
